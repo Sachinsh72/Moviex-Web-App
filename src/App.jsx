82afb84705910a3d6dd21115c3ec58bd3138c48f
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-09-13 19:49:49 +0530

    react redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-
+import { useEffect } from 'react'
 import './App.css'
+import { fetchDataFromApi } from './utils/api'
 
 function App() {
+
+
+  useEffect(() => {
+ apiTesting();
+  },[ ])
+
+  const apiTesting = () =>{
+    fetchDataFromApi('/movie/popular')
+    .then((res) => {
+      console.log(res);
+    })
+  }
 
 
   return (
