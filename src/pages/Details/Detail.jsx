2a1d0a294d54a44d6c55d876048e1f171f70c380
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-09-29 22:06:02 +0530

    detail page

--- src/pages/Details/Detail.jsx
+++ src/pages/Details/Detail.jsx
@@ -1,10 +1,27 @@
-import './detail.css'
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import Cast from "./Cast/Cast";
+import DetailsBanner from "./DetailsBanner/DetailsBanner";
 
-function Detail(){
+// import VideosSection from "./videosSection/VideosSection";
+// import Similar from "./carousels/Similar";
+// import Recommendation from "./carousels/Recommendation";
+
+function Detail({ video, crew }){
+    const { mediaType, id } = useParams();
+    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data: credits, loading: creditsLoading } = useFetch(
+        `/${mediaType}/${id}/credits`
+    );
+    
     return(
-        <>
-        
-        </>
+        <div>
+            <DetailsBanner crew={credits?.crew} />
+            <Cast data={credits?.cast} loading={creditsLoading} />
+            {/* <VideosSection data={data} loading={loading} /> */}
+            {/* <Similar mediaType={mediaType} id={id} /> */}
+            {/* <Recommendation mediaType={mediaType} id={id} /> */}
+        </div>
     )
 }
 
